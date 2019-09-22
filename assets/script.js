21570b19410354d2bf3481c22fb495a92a7d96f8
Yu-Ying Lee <yu-yinglee@Yu-Yings-MacBook-Pro.local>
2019-09-22 11:10:52 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@
 
 	
 	for (var i = 0; i < 20; i++) {
-		var changeAngle = (360/i);
+		//var changeAngle = (360/i);
 		var myLines = "";
 		//myContainer.innerHTML += '<div class="patternUnit" style="transform: rotate(45deg);"></div>'
 		//var changeColor = 255-(i/19)*255;
@@ -12,8 +12,9 @@
 		// var myTri = `<div class="triangle-right" style="background-color:rgb(${changColor},0,0);"></div>`;
 			//myLine.innerHTML += '<div class="triangle-right" style="background-color: rgb(${changingTriColor},0,${changingTriColor}; transform: rotate('+ (90 + (10*i)) +'deg);"></div>';
 			for(var j=0; j<19; j++){
-				var changeColor = 255-(j/19)*255;
-				myLines += '<div class="patternUnit" style="background-color: rgb('+ (255 - (j/10)*255) +',0,'+ (j/10)*255 +'); transform: rotate('+ (90 + (10*j)) +'deg);"></div>';
+				var changeColor = (j/19)*255;
+				var changeAngle = 90 + (10*j);
+				myLines += `<div class="patternUnit" style="background-color: rgb(${255-changeColor},0,${changeColor}); transform: rotate(${changeAngle}deg);"></div>`;
 			}
 		
 		// myLine.innerHTML += '<div class="patternUnit" style="background-color: rgb(${changingColor},0,${changingColor}; transform: rotate('+ (90 + (10*i)) +'deg);"></div>';
